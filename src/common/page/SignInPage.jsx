3b01499fcd814549a2200d72641ffbd2f2d42411
youthdonut <dbtmdgus14@naver.com>
2025-06-30 09:44:15 +0900

    feat / initalsetting 오류 수정

--- src/common/page/SignInPage.jsx
+++ src/common/page/SignInPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { loginUser } from '../store/authSlice';
+import { loginUser } from '../../store/authSlice';
 
 function SignInPage() {
   const navigate = useNavigate();
