5b932cd3ab6f19ca904e63c500079c92b68f9c4d
Sueyoung <suekim0294@gmail.com>
2025-07-01 18:29:32 +0900

    adminRoutes 및 admin 관련 파일들 수정

---
+++ src/app/admin/AllOrderPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AllOrderPage() {
+  return (
+    <div>
+      <h2>모든 주문 조회 페이지 입니다.</h2>
+    </div>
+  );
+}
+
+export default AllOrderPage;
---
+++ src/app/admin/MenuRecommendPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuRecommendPage() {
+  return (
+    <div>
+      <h2>메뉴 추천 페이지 입니다.</h2>
+    </div>
+  );
+}
+
+export default MenuRecommendPage;
