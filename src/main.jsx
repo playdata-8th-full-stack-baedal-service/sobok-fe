ae3756ba101a51d0e9eab565902d45ff4de102e8
youthdonut <dbtmdgus14@naver.com>
2025-07-01 11:17:43 +0900

    feat : 불필요한 파일 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/store';
 import './common/style/style.scss';
-import UserSignUp from './app/user/UserSignUp';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <UserSignUp />
+    <App />
   </Provider>
 );
