e46a38ab8fd33d5f51509dfa4851d34ed90cc272
Devy17 <devyks107@gmail.com>
2025-07-04 18:52:33 +0900

    feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬 feat : toss pay 만듬

--- src/pages/user/All/Main/MainPage.jsx
+++ src/pages/user/All/Main/MainPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { CheckoutPage } from '../../Pay/toss/Checkout';
+import Button from '../../../auth/SignUp/UserSignUp/components/common/Button';
 
 function MainPage() {
+
   return (
     <div>
       <h2>메인페이지 입니다.</h2>
+      <h1> 결제 수단 </h1>
+      <CheckoutPage />
     </div>
   );
 }
