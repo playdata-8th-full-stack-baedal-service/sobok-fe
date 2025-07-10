777b3925b7ff2ef130a9be8c27aee9d61ee9734f
Sueyoung <suekim0294@gmail.com>
2025-07-10 11:28:17 +0900

    rider main page 구현

---
+++ src/pages/rider/AcceptedList/AcceptedListPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AcceptedListPage() {
+  return (
+    <div>
+      <h2>수락한 요청 조회 페이지 입니다.</h2>
+    </div>
+  );
+}
+
+export default AcceptedListPage;
--- src/router/RiderRoutes.jsx
+++ src/router/RiderRoutes.jsx
@@ -3,10 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 
 // // 라이더용 페이지 컴포넌트
 import MainPage from '../pages/rider/Main/MainPage';
-// import RiderInfoPage from '../pages/rider/RiderInfo/RiderInfoPage';
-// import DelivaryHistoryPage from '../pages/rider/DelivaryHistory/DelivaryHistoryPage';
-// import RequestListPage from '../pages/rider/RequestList/RequestListPage';
-// import AcceptedListPage from '../pages/rider/AcceptedList/AcceptedListPage';
+import RiderInfoPage from '../pages/rider/RiderInfo/RiderInfoPage';
+import DelivaryHistoryPage from '../pages/rider/DelivaryHistory/DelivaryHistoryPage';
+import RequestListPage from '../pages/rider/RequestList/RequestListPage';
+import AcceptedListPage from '../pages/rider/AcceptedList/AcceptedListPage';
 
 import RiderHeader from '../layout/headers/RiderHeader';
 
@@ -16,10 +16,10 @@ function RiderRoutes() {
       <RiderHeader />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        {/* <Route path="/info" element={<RiderInfoPage />} />
+        <Route path="/info" element={<RiderInfoPage />} />
         <Route path="/history" element={<DelivaryHistoryPage />} />
         <Route path="/request-list" element={<RequestListPage />} />
-        <Route path="/accepted-list" element={<AcceptedListPage />} /> */}
+        <Route path="/accepted-list" element={<AcceptedListPage />} />
       </Routes>
     </>
   );
