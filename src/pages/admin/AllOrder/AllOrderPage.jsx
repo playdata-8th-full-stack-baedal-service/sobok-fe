58b80020b480d0d48d81b88c312aa80dfe947de8
Devy17 <devyks107@gmail.com>
2025-07-15 09:14:39 +0900

    feat : OrderGrid 생성 중

--- src/pages/admin/AllOrder/AllOrderPage.jsx
+++ src/pages/admin/AllOrder/AllOrderPage.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import Button from '../../../common/components/Button';
+import axiosInstance from '../../../services/axios-config';
+import { openModal } from '../../../store/modalSlice';
 
 function AllOrderPage() {
+  const dispatch = useDispatch();
+
+  const [orders, setOrders] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+  const [numOfRows] = useState(10);
+  const [isFullLoaded, setIsFullLoaded] = useState(false);
+
+  const formattedDate = orderDate => {
+    const date = new Date(orderDate);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  };
+
+  // 주문 내역 조회
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const response = await axiosInstance.get('/admin-service/admin/orders', {
+        params: {
+          page: pageNo,
+          size: numOfRows,
+        },
+      });
+      setOrders(prev => [...prev, ...response.data.data.content]);
+      if (response.data.data.content.length < numOfRows) {
+        setIsFullLoaded(true);
+      }
+    };
+    fetchOrders();
+  }, [pageNo, numOfRows]);
+
+  // 주문 상세보기 클릭
+  const handleOrderDetailClick = order => {
+    dispatch(
+      openModal({
+        type: 'ADMIN_ORDER_DETAIL',
+        props: {
+          order,
+        },
+      })
+    );
+  };
+
   return (
     <div>
-      <h2>모든 주문 조회 페이지 입니다.</h2>
+      <h1>주문 내역</h1>
+      {orders.map((order, index) => (
+        <div key={order.orderId}>
+          <div>
+            <span>{index + 1}/</span>
+            <span>{order.orderId}/</span>
+            <span>{formattedDate(order.createdAt)}</span>
+          </div>
+          <div>
+            <Button onClick={() => handleOrderDetailClick(order)}>주문 상세보기</Button>
+          </div>
+        </div>
+      ))}
+      {!isFullLoaded && (
+        <div>
+          <Button onClick={() => setPageNo(pageNo + 1)}>더보기 +</Button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/pages/admin/AllOrder/component/OrderGrid.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../../../store/modalSlice';
+import Button from '../../../../common/components/Button';
+
+const OrderGrid = ({ orders }) => {
+  const dispatch = useDispatch();
+
+  const formattedDate = orderDate => {
+    const date = new Date(orderDate);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  };
+
+  // 주문 상세보기 클릭
+  const handleOrderDetailClick = order => {
+    dispatch(
+      openModal({
+        type: 'ADMIN_ORDER_DETAIL',
+        props: {
+          order,
+        },
+      })
+    );
+  };
+  return (
+    <div>
+      {orders.map((order, index) => (
+        <div key={order.orderId}>
+          <div>
+            <span>{index + 1}/</span>
+            <span>{order.orderId.toUpperCase()}/</span>
+            <span>{formattedDate(order.createdAt)}</span>
+          </div>
+          <div>
+            <Button onClick={() => handleOrderDetailClick(order)}>주문 상세보기</Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+OrderGrid.propTypes = {
+  orders: PropTypes.arrayOf(
+    PropTypes.shape({
+      orderId: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default OrderGrid;
