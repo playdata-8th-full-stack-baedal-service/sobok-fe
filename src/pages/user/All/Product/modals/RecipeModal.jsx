40d4ae08531d24771c9dd39ff445e7f39ca7cf0e
Sueyoung <suekim0294@gmail.com>
2025-07-30 12:56:09 +0900

    fix : 레시피 조회 모달 수정

--- src/pages/user/All/Product/modals/RecipeModal.jsx
+++ src/pages/user/All/Product/modals/RecipeModal.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
 import ModalWrapper from '../../../../../common/modals/ModalWrapper';
-import styles from './RecipeModal.module.scss'; // SCSS 연결
+import commonStyles from '../../../../../common/forms/Post/PostContent.module.scss'; // 공통 스타일 불러오기
+import styles from './RecipeModal.module.scss'; // 모달 전용 스타일
 
 const RecipeModal = ({ onClose, recipe }) => {
   return (
-    <ModalWrapper title="레시피" onClose={onClose}>
-      <div className={styles.contatiner}>
-        <div className={styles.tiptap} dangerouslySetInnerHTML={{ __html: recipe }} />
+    <ModalWrapper title="레시피" onClose={onClose} size="long">
+      <div className={styles.container}>
+        <div
+          className={`${styles.tiptap} ${commonStyles.postContent}`}
+          dangerouslySetInnerHTML={{ __html: recipe }}
+        />
       </div>
     </ModalWrapper>
   );
