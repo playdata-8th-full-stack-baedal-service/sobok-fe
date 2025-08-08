48415bac47fd0dddd37de28039b3819083c9e427
Devy17 <devyks107@gmail.com>
2025-08-08 10:49:50 +0900

    email local change

--- src/common/forms/Email/EmailSection.jsx
+++ src/common/forms/Email/EmailSection.jsx
@@ -61,18 +61,18 @@ function EmailSection({
 
   const handleEmailLocalChange = e => {
     const value = e.target.value.replace(/[^A-Za-z0-9._%+-]/g, '');
+
+    // Show error toast if invalid characters were removed
+    if (value !== e.target.value) {
+      showNegative('도메인에는 영문, 숫자, 점(.), 하이픈(-)만 사용할 수 있습니다.');
+    }
+
     dispatch(clearEmailCheck());
     onEmailLocalChange({ ...e, target: { ...e.target, value } });
   };
 
   const handleCustomDomainChange = e => {
     const value = e.target.value.replace(/[^A-Za-z0-9.-]/g, '');
-    
-    // Show error toast if invalid characters were removed
-    if (value !== e.target.value) {
-      showNegative('도메인에는 영문, 숫자, 점(.), 하이픈(-)만 사용할 수 있습니다.');
-    }
-    
     dispatch(clearEmailCheck());
     onCustomDomainChange({ ...e, target: { ...e.target, value } });
   };
